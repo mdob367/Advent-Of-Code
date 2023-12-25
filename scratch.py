64a08793a117cbb736ba81525aaa8f4e1ba0897d
mdob367 <mattdobrin@gmail.com>
2023-12-24 20:52:16 -0800

    Git Ignore update

--- scratch.py
+++ scratch.py
@@ -1,23 +1,42 @@
-import numpy as np
+class Range4D:
+    def __init__(self, a_range, b_range, c_range, d_range):
+        self.a_range = a_range
+        self.b_range = b_range
+        self.c_range = c_range
+        self.d_range = d_range
 
-def array_to_string(arr):
-    return ''.join(arr)
+    def apply_rule(self, rule_range):
+        # Intersect each range with the rule's range
+        self.a_range = self.intersect_ranges(self.a_range, rule_range.a_range)
+        self.b_range = self.intersect_ranges(self.b_range, rule_range.b_range)
+        self.c_range = self.intersect_ranges(self.c_range, rule_range.c_range)
+        self.d_range = self.intersect_ranges(self.d_range, rule_range.d_range)
+        print('Size:', self.size())
 
-def split_string(s, delimiter):
-    return s.split(delimiter)
 
-def reassemble_string(segments, delimiter):
-    return delimiter.join(segments)
+    def size(self):
+        # Return the size of the 4D space
+        return (self.a_range[1] - self.a_range[0] + 1) * (self.b_range[1] - self.b_range[0] + 1) * (self.c_range[1] - self.c_range[0] + 1) * (self.d_range[1] - self.d_range[0] + 1)
 
-# Example usage
-arr = np.array(['O', '.', 'O', '#', '.', 'O', '.', '#', '#', 'O', '.', '.'])
-arr_str = array_to_string(arr)
+    @staticmethod
+    def intersect_ranges(range1, range2):
+        # Find the intersection of two ranges
+        return (max(range1[0], range2[0]), min(range1[1], range2[1]))
 
-# Split
-split_arr = split_string(arr_str, '#')
-print("Split Array:", split_arr)
+# Initialize the 4D space
+space = Range4D((1, 4000), (1, 4000), (1, 4000), (1, 4000))
 
-# Reassemble
-reassembled_str = reassemble_string(split_arr, '#')
-reassembled_arr = np.array(list(reassembled_str))
-print("Reassembled Array:", reassembled_arr)
+# Apply Rule 1
+rule1 = Range4D((1, 100), (500, 1000), (1, 4000), (1, 4000))
+space.apply_rule(rule1)
+
+# Apply Rule 2
+rule2 = Range4D((50, 500), (100, 200), (1000, 2000), (1, 10))
+space.apply_rule(rule2)
+
+# Apply Rule 2
+rule2 = Range4D((1, 4000), (1, 4000), (1000, 2000), (1, 10))
+space.apply_rule(rule2)
+
+# Print the resulting ranges
+print(f"Resulting Ranges: a={space.a_range}, b={space.b_range}, c={space.c_range}, d={space.d_range}")
